1e4c7d4a6a2c455419723eb5cc2f65bf2a03fcff
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-06-25 17:24:47 +1000

    Update

--- pages/api/add_email.ts
+++ pages/api/add_email.ts
@@ -1,14 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import dbConnect from '../../utils/dbConnect';
 
 export default function addEmail(req : NextApiRequest, res : NextApiResponse) {
-    dbConnect('SHOW TABLES', (error, result) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(result);
-        }
-    });
 
   res.status(200).json({ name: 'John Doe' })
 }
