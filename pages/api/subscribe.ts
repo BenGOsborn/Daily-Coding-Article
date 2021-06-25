31bed1ea3b859b6cc755ec19ea3d3e60335975ea
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-06-26 07:58:26 +1000

    Update

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import DB from '../../utils/db';
 import emailSchema from '../../joiSchema/emailSchema';
+import { v4 as uuidv4 } from 'uuid';
 
 export default async function subscribe(req : NextApiRequest, res : NextApiResponse) : Promise<void> {
     if (req.method === 'POST') {
@@ -16,17 +17,20 @@ export default async function subscribe(req : NextApiRequest, res : NextApiRespo
         // Initialize the database
         const db = new DB();
 
+        // Generate a userID to be inserted
+        const userID = uuidv4();
+
         // Add the new email if it doesnt exist
-        const inserted = await db.query("INSERT INTO emails (email) SELECT CAST($1 AS VARCHAR) WHERE NOT EXISTS (SELECT email FROM emails WHERE email = $1)", [email]);
+        const inserted = await db.query("INSERT INTO emails (email, user_id) SELECT CAST($1 AS VARCHAR), $2 WHERE NOT EXISTS (SELECT email FROM emails WHERE email = $1)", [email, userID]);
         if (inserted.rowCount === 0) {
-            return res.status(400).end("This email already exists");
+            return res.status(500).end("Failed to subscribe email");
         }
 
         // Close the connection
         db.close();
 
         // Return success
-        return res.status(200).end("Successfully added email!")
+        return res.status(200).end("Successfully subscribed email!")
 
     } else {
         return res.status(400).end("Invalid method");
--- pages/api/unsubscribe.ts
+++ pages/api/unsubscribe.ts
@@ -4,27 +4,27 @@ import DB from '../../utils/db';
 export default async function subscribe(req : NextApiRequest, res : NextApiResponse) {
     if (req.method === 'DELETE') {
         // Get the email from the request
-        const { email } : { email : string } = req.body;
+        const { userID } : { userID : string } = req.body;
 
         // Check that the email is specified in the request
-        if (typeof email === typeof undefined) {
-            return res.status(400).end("Email to be unsubscribed is required");
+        if (typeof userID === typeof undefined) {
+            return res.status(400).end("Invalid user ID");
         }
 
         // Initialize the database connection
         const db = new DB();
 
         // Attempt to delete the item from the database if it exists
-        const deleted = await db.query("DELETE FROM emails WHERE email = $1", [email]);
+        const deleted = await db.query("DELETE FROM emails WHERE user_id = $1", [userID]);
         if (deleted.rowCount === 0) {
-            return res.status(400).end("Email doesn't exist");
+            return res.status(500).end("Failed to unsubscribe email");
         }
 
         // Close the connection
         db.close();
 
         // Return success
-        return res.status(200).end("Successfully removed email");
+        return res.status(200).end("Successfully unsubscribed email");
 
     } else {
         return res.status(400).end("Invalid method");
