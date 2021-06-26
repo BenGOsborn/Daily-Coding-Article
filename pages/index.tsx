7ce138983bfac6f87a0ebf3749285f2655152dae
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-06-26 22:21:55 +1000

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import styles from '../styles/Home.module.css'
 
-const Index = () => {
+const Index : React.FC = () => {
     return (
         <div className={styles.container}>
             <h1>Subscribe</h1>
