7e39a9bd988e52558fe064dd27bb6663a4e2e457
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-06-28 09:31:42 +1000

    Update

--- README.md
+++ README.md
@@ -6,4 +6,3 @@
  - Add head tags
  - Make email template
  - Add environment variables to Heroku
- - Make sure app can push to Heroku properly (weird comma errors ? )
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     reactStrictMode: true,
     siteURL:
-        process.env.NODE !== "development"
-            ? `http://localhost:${process.env.PORT}`
-            : "https://siteurl.com",
+        process.env.NODE_ENV !== "development"
+            ? `http://localhost:${process.env.PORT || 3000}`
+            : "https://daily-coding-article.herokuapp.com",
 };
