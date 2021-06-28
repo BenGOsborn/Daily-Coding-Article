61fa1f33c2431f70ac56c40e339115136ecf8841
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-06-28 15:48:23 +1000

    Update

--- README.md
+++ README.md
@@ -3,7 +3,4 @@
 ## An email subscription service where users are sent a new coding article everyday so they can learn something new everyday.
 
 ### TODO
- - Make email template
- - Add favicon and manifest
  - Add environment variables to Heroku
- - Add a service worker ?
--- emailTemplates/compileTemplate.ts
+++ emailTemplates/compileTemplate.ts
@@ -8,12 +8,28 @@ export interface TemplateContent {
     unsubscribe: string;
 }
 
+interface TemplateData extends Omit<TemplateContent, "body"> {
+    body: string[];
+}
+
 export default async function compileTemplate(
     content: TemplateContent,
     templateFile: string = "emailTemplates/template.ejs"
 ): Promise<string> {
+    // Modify the body
+    const newBody = content.body
+        .trim()
+        .split("\n\n")
+        .filter((value) => value !== "");
+    const templateData: TemplateData = {
+        title: content.title,
+        body: newBody,
+        articleURL: content.articleURL,
+        unsubscribe: content.unsubscribe,
+    };
+
     // Compile the template with the data
-    const html = await ejs.renderFile(templateFile, content);
+    const html = await ejs.renderFile(templateFile, templateData);
 
     // Return the html
     return html;
